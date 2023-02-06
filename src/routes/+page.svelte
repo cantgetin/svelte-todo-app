664f99ff090e3524ad0257a5b879ceff7c10885f
cantgetin <84032933+cantgetin@users.noreply.github.com>
2023-02-06 17:17:33 +0500

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import TodoList from "./TodoList.svelte";
+</script>
+
+<div class="app">
+    <TodoList/>
+</div>
+
+<style>
+    :global(*) {
+        padding: 0;
+        margin: 0;
+    }
+</style>
---
+++ src/routes/TodoList.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    import {list} from '../store/todo'
+    import TodoAdd from "./TodoAdd.svelte";
+</script>
+
+<TodoAdd/>
+
+<div class="item-list">
+    {#each $list as item}
+        <div class="item-list--item">
+            <div>{item.title}</div>
+            <div>{item.created}</div>
+        </div>
+    {/each}
+</div>
+
+<style>
+    .item-list {
+        display: flex;
+        flex-wrap: wrap;
+        padding: 10px;
+        gap: 10px;
+    }
+
+    .item-list--item {
+        width: 10vw;
+        border: 1px solid black;
+        height: fit-content;
+        background: white;
+    }
+</style>
